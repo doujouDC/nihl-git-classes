069ad32611c92fc2a30122f32e9e6033557d8cac
DJ Joubert <doujou.dc@gmail.com>
2023-10-23 10:58:58 -0400

    created a script and and added some folder and downloaded an image

---
+++ doug-script.R
@@ -0,0 +1,15 @@
+# I want you to install the gapminder package and "turn in on"
+
+install.packages("gapminder")
+library(gapminder)
+
+install.packages("tidyverse")
+library(tidyverse)
+
+read_data <- gapminder
+
+# Let us add some folders
+
+dir.create("images")
+dir.create("docs")
+dir.create("figures")
